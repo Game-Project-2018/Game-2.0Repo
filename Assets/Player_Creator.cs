1d81a5809afb1eccbce65781a79210e00529f98f
Matemburg <VonMatemburg@gmail.com>
2018-11-29 22:53:25 +0100

    fix Char

--- Assets/Player_Creator.cs
+++ Assets/Player_Creator.cs
@@ -9,11 +9,11 @@ public class Player_Creator : MonoBehaviour {
 
 	static int i=0;
 	void Start () {
-		Mesh drunk = (Mesh)AssetDatabase.LoadAssetAtPath ("Assets/Obj/drunk.obj",typeof(Mesh));
+		//Mesh drunk = (Mesh)AssetDatabase.LoadAssetAtPath ("Assets/Obj/drunk.obj",typeof(Mesh));
 		if (i >= Data_static.Player_tab.Count)
 			return;
-		else if (Data_static.Player_tab [i].name == "drunk")
-			this.GetComponent<MeshFilter> ().mesh = drunk;
+		else if (Data_static.Player_tab [i].name != "drunk")
+			Destroy(GetComponent<GameObject>());
 	}
 	
 
