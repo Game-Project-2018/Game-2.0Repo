256b96230a6aba10e1b41ecd9cf6b73a64c79396
Mateusz <38332261+Matemburg@users.noreply.github.com>
2018-12-14 17:50:54 +0100

    Slider HP / Char select upgrade

--- Assets/Character_Select.cs
+++ Assets/Character_Select.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Character_Select : MonoBehaviour {
 
-	public bool drunk=false,doctor=false;
+	public bool drunk=false,doctor=false, Survivalist=false, Soldier= false,Murderer=false;
 
 	void Start(){
 		Data_static.Player_tab = new List <Player_stats> ();
@@ -35,23 +35,66 @@ public class Character_Select : MonoBehaviour {
         {
             for (int i = 0; i <= Data_static.Player_tab.Count; i++)
             {
-                if (Data_static.Player_tab[i].name == "doctor") ;
+                if (Data_static.Player_tab[i].name == "doctor") 
                     Data_static.Player_tab.RemoveAt(i);
             }
         }
         Debug.Log(Data_static.Player_tab[0].name);
     }
 
-   
+    public void Survivalist_toggle(bool newValue)
+    {
+        Survivalist = newValue;
+        if (Survivalist)
+        {
+            Data_static.Player_tab.Add(new Player_stats("survivalist"));
+        }
+        else
+        {
+            for (int i = 0; i <= Data_static.Player_tab.Count; i++)
+            {
+                if (Data_static.Player_tab[i].name == "survivalist") 
+                Data_static.Player_tab.RemoveAt(i);
+            }
+        }
+        Debug.Log(Data_static.Player_tab[0].name);
+    }
 
-    public void StartGame()
-	{
-		Data_static.Player_tab = new List <Player_stats> ();
-		if (drunk) {
-			Data_static.Player_tab.Add(new Player_stats("drunk"));
-		}
-		if (doctor) {
-			Data_static.Player_tab.Add(new Player_stats("doctor"));
-		}
-	}
+    public void Soldier_toggle(bool newValue)
+    {
+        Soldier = newValue;
+        if (Soldier)
+        {
+            Data_static.Player_tab.Add(new Player_stats("soldier"));
+        }
+        else
+        {
+            for (int i = 0; i <= Data_static.Player_tab.Count; i++)
+            {
+                if (Data_static.Player_tab[i].name == "soldier") 
+                Data_static.Player_tab.RemoveAt(i);
+            }
+        }
+        Debug.Log(Data_static.Player_tab[0].name);
+    }
+
+    public void Murderer_toggle(bool newValue)
+    {
+        Murderer = newValue;
+        if (Murderer)
+        {
+            Data_static.Player_tab.Add(new Player_stats("murderer"));
+        }
+        else
+        {
+            for (int i = 0; i <= Data_static.Player_tab.Count; i++)
+            {
+                if (Data_static.Player_tab[i].name == "murderer")
+                    Data_static.Player_tab.RemoveAt(i);
+            }
+        }
+        Debug.Log(Data_static.Player_tab[0].name);
+    }
+
+ 
 }
---
+++ Assets/HpShow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpShow : MonoBehaviour {
+
+    public Slider HPSliderPlayer;
+    public Text HPtextPlayer;
+
+    public Slider HPSliderHighlighted;
+    public Text HPtextHighlighted;
+
+    public GameObject ActivePlayer;
+
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+        HPSliderPlayer.maxValue = ActivePlayer.GetComponent<AcctivePlayer>().Player.GetComponent<BaseStats>().MaxHP;
+        HPSliderPlayer.value = ActivePlayer.GetComponent<AcctivePlayer>().Player.GetComponent<BaseStats>().HP;
+        HPtextPlayer.text = ActivePlayer.GetComponent<AcctivePlayer>().Player.GetComponent<BaseStats>().HP.ToString() + " /" + ActivePlayer.GetComponent<AcctivePlayer>().Player.GetComponent<BaseStats>().MaxHP;
+    }
+}
