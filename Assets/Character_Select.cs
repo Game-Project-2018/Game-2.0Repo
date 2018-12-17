1b3ec3b3e41e31d817d59a57d979fc74a0a913ea
Mateusz <38332261+Matemburg@users.noreply.github.com>
2018-12-17 16:24:51 +0100

    Char_Explorer 0.9

--- Assets/Character_Select.cs
+++ Assets/Character_Select.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Character_Select : MonoBehaviour {
 
 	public bool drunk=false,doctor=false, Survivalist=false, Soldier= false,Murderer=false;
-    public GameObject drunkG, doctorG, SurvivalistG, SoldierG, MurdererG;
+    public GameObject drunkG, doctorG, SurvivalistG, SoldierG, MurdererG,MotherG;
     void Start(){
 		Data_static.Player_tab = new List <Player_stats> ();
-	}
+        drunkG.SetActive(false);
+        doctorG.SetActive(false);
+        SurvivalistG.SetActive(false);
+        SoldierG.SetActive(false);
+      //  MurdererG.SetActive(false);
+        MotherG.SetActive(false);
+    }
 
 	public void drunk_toggle(bool newValue)
 	{
@@ -27,6 +33,29 @@ public class Character_Select : MonoBehaviour {
 		Debug.Log (Data_static.Player_tab [0].name);
         
 	}
+
+
+    public void mother_toggle(bool newValue)
+    {
+
+        drunk = newValue;
+        if (drunk)
+        {
+            Data_static.Player_tab.Add(new Player_stats("mother"));
+            MotherG.SetActive(true);
+        }
+        else
+        {
+            for (int i = 0; i < Data_static.Player_tab.Count; i++)
+            {
+                if (Data_static.Player_tab[i].name == "mother")
+                    Data_static.Player_tab.RemoveAt(i);
+            }
+            MotherG.SetActive(false);
+        }
+       
+
+    }
 
     public void doctor_toggle(bool newValue)
     {
--- Assets/Player_Creator.cs
+++ Assets/Player_Creator.cs
@@ -8,24 +8,71 @@ public class Player_Creator : MonoBehaviour {
 
 
 	static int i=0;
-	void Start () {
-		//Mesh drunk = (Mesh)AssetDatabase.LoadAssetAtPath ("Assets/Obj/drunk.obj",typeof(Mesh));
-		//if (i >= Data_static.Player_tab.Count)
-			//return;
-		//Debug.Log(Data_static.Player_tab.Count);
-		if (Data_static.Player_tab.Count == 0) {
-            GetComponent<BaseStats>().HP = -1;
-            Debug.Log("Delete");
-            Destroy(gameObject);
-        }
-        else
-        {
-            GetComponent<BaseStats>().HP = GetComponent<BaseStats>().MaxHP;
-        }
-		 //if (Data_static.Player_tab[i].name != "drunk")
-			
+    public GameObject Survivalist;
+    public GameObject Drunk;
+    public GameObject Doctor;
+    public GameObject Mother;
+    public GameObject Soldier;
+    void Awake() {
+         bool drunk = false, doctor = false, survivalist = false, soldier = false, mother = false;
 
-	}
-	
+        for (int i=0;i<Data_static.Player_tab.Count;i++)
+        {
+            if (Data_static.Player_tab[i].name == "survivalist")
+            {
+                Activate(Survivalist,i);
+                survivalist = true;
+            }
+            else if (Data_static.Player_tab[i].name == "doctor")
+            {
+                Activate(Doctor, i);
+                doctor = true;
+            }
+            else if (Data_static.Player_tab[i].name == "drunk")
+            {
+                Activate(Drunk, i);
+                drunk = true;
+            }
+            else if (Data_static.Player_tab[i].name == "soldier")
+            {
+                Activate(Soldier, i);
+                soldier = true;
+            }
+            else if (Data_static.Player_tab[i].name == "mother")
+            {
+                Activate(Mother, i);
+                mother = true;
+            }
+
+        }
+        if (!survivalist)
+            Deactivate(Survivalist);
+        if (!doctor)
+            Deactivate(Doctor);
+        if (!drunk)
+            Deactivate(Drunk);
+        if (!soldier)
+            Deactivate(Soldier);
+        if (!mother)
+            Deactivate(Mother);
+         //if (Data_static.Player_tab[i].name != "drunk")
+
+
+    }
+
+    void Deactivate(GameObject G)
+    {
+        G.GetComponent<BaseStats>().HP = -1;
+        Destroy(G);
+
+
+    }
+
+    void Activate(GameObject G,int i)
+    {
+        G.SetActive(true);
+        G.GetComponent<BaseStats>().HP = Data_static.Player_tab[i].HP;
+    }
+
 
 }
