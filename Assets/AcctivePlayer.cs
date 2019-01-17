5c4ded64e7226608533add1f63dcf44889df74e5
Mateusz <38332261+Matemburg@users.noreply.github.com>
2019-01-17 17:45:47 +0100

    Camera fix

--- Assets/AcctivePlayer.cs
+++ Assets/AcctivePlayer.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AcctivePlayer : MonoBehaviour {
-
+    public Camera cam;
 	 public GameObject Player;
 	// Use this for initialization
 	void Start () {
 		Player = null;
+       
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LateUpdate () {
+        if (Player != null)
+            cam.transform.LookAt(Player.transform.position);
+    }
 }
--- Assets/Camera_scroll.cs
+++ Assets/Camera_scroll.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Camera_scroll : MonoBehaviour {
     Camera cam;
+   // public GameObject player;
+   // GameObject acctivePlayer;
+   // public float x;
+   // public float z;
     // Use this for initialization
     void Start () {
         cam = this.GetComponent<Camera>();
+       // acctivePlayer = player.GetComponent<AcctivePlayer>().Player;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+     
+        
         if (Input.GetAxis("Mouse ScrollWheel") != 0f) 
         {
             cam.orthographicSize += (Input.GetAxis("Mouse ScrollWheel") * -1);
